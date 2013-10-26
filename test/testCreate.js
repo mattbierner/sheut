e414514236a0519c31b37946799ca64b537873b6
Matt Bierner <mattbierner@gmail.com>
2013-10-26 13:23:36 -0400

    Cleaning up operation interface

--- test/testCreate.js
+++ test/testCreate.js
@@ -14,20 +14,36 @@ function(run, step, operations){
             ["Step",
             function(){
                 var d = run.beginFromInput("var a, b; a = 3; debugger; b = 4; debugger; a = 5;");
-                assert.equal(operations.evaluateInput(d, "typeof a").value, 'undefined');
-                assert.equal(operations.evaluateInput(d, "typeof b").value, 'undefined');
+                assert.equal(
+                    operations.execute(d, operations.evaluateInput("typeof a")).value,
+                    'undefined');
+                assert.equal(
+                    operations.execute(d, operations.evaluateInput("typeof b")).value,
+                    'undefined');
                 
                 var d1 = step.run(d);
-                assert.equal(operations.evaluateInput(d1, "a").value, 3);
-                assert.equal(operations.evaluateInput(d1, "b").value, undefined);
+                assert.equal(
+                    operations.execute(d1, operations.evaluateInput("a")).value,
+                    3);
+                assert.equal(
+                    operations.execute(d1, operations.evaluateInput("b")).value,
+                    undefined);
 
                 var d2 = step.run(d1);
-                assert.equal(operations.evaluateInput(d2, "a").value, 3);
-                assert.equal(operations.evaluateInput(d2, "b").value, 4);
+                assert.equal(
+                    operations.execute(d2, operations.evaluateInput("a")).value,
+                    3);
+                assert.equal(
+                    operations.execute(d2, operations.evaluateInput("b")).value,
+                    4);
                 
                 var d3 = step.run(d2);
-                assert.equal(operations.evaluateInput(d3, "a").value, 5);
-                assert.equal(operations.evaluateInput(d3, "b").value, 4);
+                assert.equal(
+                    operations.execute(d3, operations.evaluateInput("a")).value,
+                    5);
+                assert.equal(
+                    operations.execute(d3, operations.evaluateInput("b")).value,
+                    4);
             }],
         ],
     };
