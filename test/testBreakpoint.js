1ab858e5448fbf0c6486c10a56afee58ccdbb8a8
Matt Bierner <mattbierner@gmail.com>
2013-10-28 00:32:28 -0400

    Fixed unconditional bp to skip if already on line

--- test/testBreakpoint.js
+++ test/testBreakpoint.js
@@ -29,6 +29,23 @@ function(breakpoint,
                     operations.execute(d2, operations.evaluateInput("x")).value,
                     3);
             }],
+            ["multi line bp only hit once",
+            function(){
+                var d = run.beginFromInput("var x=0 \n x=1; \n x=2; x=3; \n x=4;");
+                
+                var bp = breakpoint.createUnconditional(0, 3);
+                d = state.addBreakpoint(d, bp);
+                
+                var d1 = step.run(d);
+                assert.equal(
+                    operations.execute(d1, operations.evaluateInput("x")).value,
+                    1);
+
+                var d2 = step.run(d1);
+                assert.equal(
+                    operations.execute(d2, operations.evaluateInput("x")).value,
+                    4);
+            }],
             
             ["Basic conditional bp",
             function(){
