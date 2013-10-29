7fb86c815851d2b73a20e6150ecd321ca8cc8bc2
Matt Bierner <mattbierner@gmail.com>
2013-10-29 15:51:50 -0400

    Changed breakpoint interfaces to use test combinators rather than breakpoint combinators.

--- test/testBreakpoint.js
+++ test/testBreakpoint.js
@@ -18,7 +18,7 @@ function(breakpoint,
             function(){
                 var d = debug.beginInput("var x=0 \n x=1; \n x=2; \n x=3;");
                 
-                var bp = breakpoint.createUnconditional(0, 3);
+                var bp = breakpoint.create(0, breakpoint.unconditional(3));
                 d = state.addBreakpoint(d, bp);
                 
                 var d1 = step.run(d);
@@ -35,7 +35,7 @@ function(breakpoint,
             function(){
                 var d = debug.beginInput("var x=0 \n x=1; \n x=2; x=3; \n x=4;");
                 
-                var bp = breakpoint.createUnconditional(0, 3);
+                var bp = breakpoint.create(0, breakpoint.unconditional(3));
                 d = state.addBreakpoint(d, bp);
                 
                 var d1 = step.run(d);
@@ -53,7 +53,8 @@ function(breakpoint,
             function(){
                 var d = debug.beginInput("var x=0 \n x=1; \n x=2; \n x=3;");
                 
-                var bp = breakpoint.createConditional(0, evaluate.evaluateInput("typeof x !== 'undefined' && x === 1"));
+                var bp = breakpoint.create(0, breakpoint.conditional(
+                    evaluate.evaluateInput("typeof x !== 'undefined' && x === 1")));
                 d = state.addBreakpoint(d, bp);
                 
                 var d1 = step.run(d);
@@ -71,12 +72,35 @@ function(breakpoint,
             function(){
                 var d = debug.beginInput("var x=0 \n x=1; \n x=2; \n x=3;");
                 
-                var bp = breakpoint.createConditional(0, evaluate.evaluateInput("d"));
+                var bp = breakpoint.create(0, breakpoint.conditional(evaluate.evaluateInput("d")));
                 d = state.addBreakpoint(d, bp);
                 
                 var d1 = step.run(d);
                 assert.equal(
                     run.extract(d1, evaluate.evaluateInput("x")).value,
+                    3);
+            }],
+            
+            ["And conditional bp",
+            function(){
+                var d = debug.beginInput("var x=0 \n x=1; \n x=2; \n x=3;");
+                
+                var bp = breakpoint.create(0,
+                    breakpoint.and(
+                        breakpoint.conditional(
+                            evaluate.evaluateInput("x > 0")),
+                        breakpoint.unconditional(3)));
+                
+                d = state.addBreakpoint(d, bp);
+                
+                var d1 = step.run(d);
+                assert.equal(
+                    run.extract(d1, evaluate.evaluateInput("x")).value,
+                    1);
+
+                var d2 = step.run(d1);
+                assert.equal(
+                    run.extract(d2, evaluate.evaluateInput("x")).value,
                     3);
             }],
         ],
