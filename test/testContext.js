8de1c141741d0e945e246a0385f42aebd6520047
Matt Bierner <mattbierner@gmail.com>
2013-10-29 20:13:14 -0400

    More documentation and clean up

--- test/testContext.js
+++ test/testContext.js
@@ -1,15 +1,38 @@
-define(['sheut/run',
+define(['sheut/breakpoint',
+        'sheut/debug',
         'sheut/step',
-        'sheut/operations',
-        'sheut/operations/evaluation'],
-function(run,
+        'sheut/run',
+        'sheut/policy',
+        'sheut/operations/evaluation',
+        'sheut/state'],
+function(breakpoint,
+        debug,
         step,
-        operations,
-        evaluation){
+        run,
+        policy,
+        evaluate,
+        state){
    
     return {
-        'module': "Create",
+        'module': "Context",
         'tests': [
+            ["Environment",
+            function(){
+                var d = debug.beginInput("var x=0 \n x=1; \n x=2; \n x=3;");
+                
+                var bp = breakpoint.create(0, breakpoint.unconditional(3));
+                d = state.addBreakpoint(d, bp);
+                
+                var d1 = step.run(d);
+                assert.equal(
+                    run.extract(d1, evaluate.evaluateInput("x")).value,
+                    1);
+
+                var d2 = step.run(d1);
+                assert.equal(
+                    run.extract(d2, evaluate.evaluateInput("x")).value,
+                    3);
+            }]
         ],
     };
 });
