bf190524bbf8f69d42a03b09c4cd73f6ce77ae4f
Matt Bierner <mattbierner@gmail.com>
2013-11-08 11:46:00 -0500

    Adding more tests for env

--- test/testContext.js
+++ test/testContext.js
@@ -24,17 +24,48 @@ function(compute,
             function(){
                 var d = debug.beginInput("var a = 0; var b = 0;");
                
-                var d1 = step.finish(step.run(d));
+                var d1 = step.run(d);
                 
                 assert.equal(
                     run.extract(d1, compute.bind(context.environment, context.getEnvironmentOuter)),
                     null);
                 
-               assert.equal(
-                    run.extract(d1, compute.bind(context.environment, function(env) {
-                        return context.getEnvironmentBinding(env, 'a');
-                    })).value,
-                    0);
+                var a = run.extract(d1, compute.bind(context.environment, function(env) {
+                    return context.getEnvironmentBinding(env, 'a');
+                }));
+                assert.equal(a.type, 'number');
+                assert.equal(a.value, 0);
+                
+                var inf = run.extract(d1, compute.bind(context.environment, function(env) {
+                    return context.getEnvironmentBinding(env, 'Infinity');
+                }));
+                assert.equal(inf.type, 'number');
+                assert.equal(inf.value, Infinity);
+                
+                // Lookup undefined binding
+                var none = run.extract(d1, compute.bind(context.environment, function(env) {
+                    return context.getEnvironmentBinding(env, 'x');
+                }));
+                assert.ok(!none);
+            }],
+            ["Outer ",
+            function(){
+                var d = debug.beginInput("var a = 0; function f() { var a = 10; debugger; } f(); ");
+               
+                var d1 = step.run(d);
+                
+                var a = run.extract(d1, compute.bind(context.environment, function(env) {
+                    return context.getEnvironmentBinding(env, 'a');
+                }));
+                assert.equal(a.type, 'number');
+                assert.equal(a.value, 10);
+                
+                var outer = run.extract(d1, compute.bind(context.environment, context.getEnvironmentOuter));
+                assert.ok(outer);
+                
+                var aOuter = run.extract(d1, context.getEnvironmentBinding(outer, 'a'));
+                assert.equal(aOuter.type, 'number');
+                assert.equal(aOuter.value, 0);
             }]
         ],
     };
